dd2bdf88ead2b3ebc4e0c9520985961fe5e673e7
dtcoops <60304831+dtcoops@users.noreply.github.com>
2021-09-01 14:23:48 -0700

    Animating Graph
    Modified points Y position based on Sin function over time

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -8,6 +8,8 @@ public class Graph : MonoBehaviour
     [SerializeField, Range(10, 100)]
     int resolution = 10;
 
+    Transform[] points;
+
     private void Awake()
     {
         /*
@@ -21,9 +23,13 @@ public class Graph : MonoBehaviour
         float step = 2f / resolution;
         Vector3 position = Vector3.zero;
         var scale = Vector3.one * step;
-        for (int i = 0; i < resolution; i++)
+
+        // keep track of points for animation
+        points = new Transform[resolution];
+
+        for (int i = 0; i < points.Length; i++)
         {
-            Transform point = Instantiate(pointPrefab);
+            Transform point = points[i] = Instantiate(pointPrefab);
             position.x = (i + 0.5f) * step - 1f;
 
             // graph type
@@ -35,6 +41,17 @@ public class Graph : MonoBehaviour
             point.localPosition = position;
             point.localScale = scale;
             point.SetParent(transform, false);
+        }
+    }
+    void Update()
+    {
+        float time = Time.time;
+        for (int i = 0; i < points.Length; i++)
+        {
+            Transform point = points[i];
+            Vector3 position = point.localPosition;
+            position.y = Mathf.Sin(Mathf.PI * (position.x + time));
+            point.localPosition = position;
         }
     }
 }
